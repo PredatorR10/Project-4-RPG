9e89d3aafad3c5fb01ae8bc7b7d1f4272bc48c08
PredatorR10 <godofgamingpr10@gmail.com>
2022-04-06 22:04:19 -0400

    registered Monsters for admin site

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Character
+from .models import Character, Monster
 
 # Register your models here.
 
 admin.site.register(Character)
+admin.site.register(Monster)
